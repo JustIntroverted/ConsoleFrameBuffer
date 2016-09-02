ee5dbf35f571d5a1e401d713a173ea4edcda95d7
JustIntroverted <jd.schar@gmail.com>
2016-09-02 14:01:14 -0500

    cleaned code using codemaid

--- ConsoleFrameBuffer.Test/Utility/Point.cs
+++ ConsoleFrameBuffer.Test/Utility/Point.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleFrameBuffer.Test.Utility {
+﻿namespace ConsoleFrameBuffer.Test.Utility {
 
     public class Point {
         public int X { get; set; }
